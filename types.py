7d2795b896a56176e10345e997f285c2e12694eb
Patrick O'Neill <paddyjoneill@hotmail.com>
2020-04-07 18:56:03 +0100

    variables

---
+++ types.py
@@ -0,0 +1,37 @@
+# print(type("Hello World!"))
+# print(type(4))
+# print(type(4.0))
+# print(type(True))
+# print(type(False))
+# print(type(None))
+
+# print("Hello World!"[0])
+# print("Hello World!"[0:5])
+
+# print("Hello" + " " + "World!")
+
+# print(len("Hello World!"))
+
+# print(3 + 4)
+# print(5 - 2)
+# print(10 / 2)
+# print(5 * 4)
+
+# print(9 % 2)
+
+# print(float(10))
+# print(str(10))
+
+# print(type("42"))
+# print(type("42.0"))
+# print(type(1 + 2.4))
+# print(type(1 == 1)) # hard!
+# print(type(2 == 4))
+
+first_name = "Patrick"
+last_name = "O'Neill"
+year_of_birth = 1984
+
+username = first_name[0:3] + last_name[0:3] + str(year_of_birth)
+
+print(username)
